320894b5cf20bc47d983dc9e06cd0baca464213a
Wafi Faisal Falah <wafifaisal19@gmail.com>
2024-12-18 10:46:05 +0700

    Backend

--- prisma/migrations/20241216061629_revise_ref_by/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "ref_by" DROP NOT NULL;
--- prisma/migrations/20241218030617_add_org_name/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Ticket" ADD COLUMN     "end_time" TIMESTAMP(3),
-ADD COLUMN     "start_time" TIMESTAMP(3);
